60ef588730d4f01ee12e09fe01001cf3eb52d3e6
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-06-04 15:49:21 +0900

    yoonsun] front-router >> 간격 수정

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,20 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import ProductPage from '../admin/product/Product';
-import App from '../App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProductPage from "../admin/product/Product";
+import App from "../App";
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />
-    },
-    {
-        path: '/admin/product',
-        element: <ProductPage />
-    }
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/admin/product",
+    element: <ProductPage />,
+  },
 ]);
 
 const Router = () => {
-    return <RouterProvider router={router} />;
-  };
+  return <RouterProvider router={router} />;
+};
 
 export default Router;
