3ff0753a5e4c6ba638f0654733ef7dbf80212788
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-06-04 15:45:57 +0900

    yoonsun] front-router >> router 설정 추가

---
+++ frontend/src/routes/Router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProductPage from '../admin/product/Product';
+import App from '../App';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    },
+    {
+        path: '/admin/product',
+        element: <ProductPage />
+    }
+]);
+
+const Router = () => {
+    return <RouterProvider router={router} />;
+  };
+
+export default Router;
