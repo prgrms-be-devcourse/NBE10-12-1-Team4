e8556eda73f4892785b7ca4042bf41d92567cb13
user <offonoff6101@naver.com>
2026-06-05 12:02:54 +0900

    feat: 주문 상태 일괄 변경 쿼리 추가

--- backend/src/main/java/com/backend/domain/order/repository/OrderRepository.java
+++ backend/src/main/java/com/backend/domain/order/repository/OrderRepository.java
@@ -1,13 +1,21 @@
 package com.backend.domain.order.repository;
 
 import com.backend.domain.order.entity.Order;
+import com.backend.domain.order.entity.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 이메일 기준 주문 조회
     List<Order> findByEmail(String email);
+
+    //
+    List<Order> findByStatusAndBatchDeadlineLessThanEqual(
+            OrderStatus status,
+            LocalDateTime batchDeadline
+    );
 
 }
