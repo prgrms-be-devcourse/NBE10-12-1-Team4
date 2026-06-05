10997c80d1ca4eef01262e4cebd7490581824d92
yoonsun9128 <113073475+yoonsun9128@users.noreply.github.com>
2026-06-05 14:24:35 +0900

    refactor: front route 통일

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import '@radix-ui/themes/styles.css'
 import './index.css'
 import App from './App.tsx'
@@ -7,8 +8,10 @@ import { CartProvider } from './store/CartContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
